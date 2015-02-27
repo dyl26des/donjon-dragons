fa7b34bb7ea1fa80a34b0762063c88d88bdabb71
dyl26des <dyl26des@gmail.com>
2015-02-27 11:35:44 +0100

    Revert "reparer"
    This reverts commit ecd5cc9309b6184ec9f25c40b2b18804cad1c31f.

--- classejs/Map.js
+++ classejs/Map.js
@@ -1,55 +1,143 @@
 function Map(nom) {
-	
-	// Création de l'objet XmlHttpRequest
+    this.personnages = new Array();
+    
+    this.chargerMap(nom);
+    
+}
+function Map(nom, joueur) {
+    this.personnages = new Array();
+    this.personnages[0] = joueur;
+    this.chargerMap(nom);
+    
+}
+
+
+Map.prototype.chargerMap = function (nom){
+    // Création de l'objet XmlHttpRequest
 	var xhr = getXMLHttpRequest();
-        this.personnages = new Array();
 
 	// Ici viendra le code que je vous présente ci-dessous
         // Chargement du fichier
     xhr.open("GET", './maps/' + nom + '.json', false);
     xhr.send(null);
-    if(xhr.readyState != 4 || (xhr.status != 200 && xhr.status != 0)) // Code == 0 en local
+    if(xhr.readyState !== 4 || (xhr.status !== 200 && xhr.status !== 0)) // Code == 0 en local
 	throw new Error("Impossible de charger la carte nommée \"" + nom + "\" (code HTTP : " + xhr.status + ").");
     var mapJsonData = xhr.responseText;
     // Analyse des données
     var mapData = JSON.parse(mapJsonData);
     
     this.tileset = new Tileset(mapData.tileset);
-    this.terrain = mapData.terrain;
-    this.decors  = mapData.decors;
-}
+    this.terrain0 = mapData.terrain0;
+    this.terrain1 = mapData.terrain1;
+    this.terrain2  = mapData.terrain2;
+    this.terrain3 = mapData.terrain3;
+    var posjoueur = mapData.posjoueur;
+    var pnjs    =   mapData.pnjs;
+    
+ 
+    this.personnages.splice(1,this.personnages.length-1);
+    this.personnages[0].repositionner (posjoueur[0],posjoueur[1],posjoueur[2]);
 
+    for (var i = 0, l = pnjs.length ; i < l; i++)
+    {
+        var pnj = pnjs[i];
+        
+        var persopnj = new Personnage (pnj[0],pnj[1],pnj[2], pnj[3]);
+        this.addPersonnage(persopnj);
+    }
+    /* terrain0 = sol
+     * terrain1 = objet au sol
+     * terrain2 = mur et collision
+     * terrain3 = plafond, superieur au personnages
+     */
+};
 // Pour récupérer la taille (en tiles) de la carte
 Map.prototype.getHauteur = function() {
-	return this.terrain.length;
+	return this.terrain0.length;
 };
 Map.prototype.getLargeur = function() {
-	return this.terrain[0].length;
-};
-Map.prototype.dessinerMap = function(context) {
-	for(var i = 0, l = this.terrain.length; i < l ; i++) {
-		var ligne = this.terrain[i];
-		var y = i * 32;
-		for(var j = 0, k = ligne.length; j < k ; j++) {
-			this.tileset.dessinerTile(ligne[j], context, j * 32, y);
-		}
-	}
-        for(var i = 0, l = this.decors.length; i < l ; i++) {
-		var ligne = this.decors[i];
-		var y = i * 32;
-		for(var j = 0, k = ligne.length; j < k ; j++) {
-			this.tileset.dessinerTile(ligne[j], context, j * 32, y);
-		}
-	}
-        for(var i = 0, l = this.personnages.length; i < l ; i++) {
-	this.personnages[i].dessinerPersonnage(context);
-        alert (this.personnages[i].image.src );
-        };
+	return this.terrain0[0].length;
 };
 
-// Pour ajouter un personnage
+Map.prototype.getTerrain2 = function( x, y){
+    var ligne=this.terrain2[y];
+    return ligne[x];
+};
+
 Map.prototype.addPersonnage = function(perso) {
 	this.personnages.push(perso);
 };
 
-//tracage des personnage au chargement de la map
+Map.prototype.dessinerMap = function(context) {
+	for(var i = 0, l = this.terrain0.length; i < l ; i++) {
+		var ligne = this.terrain0[i];
+		var y = i * 32;
+		for(var j = 0, k = ligne.length; j < k ; j++) {
+			this.tileset.dessinerTile(ligne[j], context, j * 32, y);
+		}
+	}
+
+        for(var i = 0, l = this.terrain2.length; i < l ; i++) {
+		var ligne = this.terrain2[i];
+		var y = i * 32;
+		for(var j = 0, k = ligne.length; j < k ; j++) {
+			this.tileset.dessinerTile(ligne[j], context, j * 32, y);
+		}
+	}
+        
+        for(var i = 0, l = this.terrain1.length; i < l ; i++) {
+		var ligne = this.terrain1[i];
+		var y = i * 32;
+		for(var j = 0, k = ligne.length; j < k ; j++) {
+			this.tileset.dessinerTile(ligne[j], context, j * 32, y);
+		}
+	}
+       
+       
+        for(var i = 0, l = this.personnages.length ; i < l ; i++) {
+	this.personnages[i].dessinerPersonnage(context,this);
+        }
+        
+        
+        for(var i = 0, l = this.terrain3.length; i < l ; i++) {
+		var ligne = this.terrain3[i];
+		var y = i * 32;
+		for(var j = 0, k = ligne.length; j < k ; j++) {
+			this.tileset.dessinerTile(ligne[j], context, j * 32, y);
+		}
+        }
+        
+        if (!isPreCombat && this.personnages[0].etatAnimation===-1 ){
+            for (var i=1, l = this.personnages.length ; i<l;i++){
+                var mob = this.personnages[i];
+
+                switch  (mob.direction) {
+                    case 0 : 
+                        if ((mob.x === this.personnages[0].x) && (mob.y < this.personnages[0].y))
+                        {
+                            mPreCombat(context,mob);
+                        }
+                    break;
+                    case 1 :                         
+                        if ((mob.y === this.personnages[0].y) && (mob.x > this.personnages[0].x))
+                        {
+                            mPreCombat(context,mob);
+                        }
+                    break;
+                    case 2 : 
+                         if ((mob.y === this.personnages[0].y) && (mob.x < this.personnages[0].x))
+                        {
+                            mPreCombat(context,mob);
+                        }
+                    break;
+                    case 3 :
+                        if ((mob.x === this.personnages[0].x) && (mob.y > this.personnages[0].y))
+                        {
+                            mPreCombat(context,mob);
+                        }
+                        
+                    break;
+                }
+            }
+        }
+};
--- classejs/rpg.js
+++ classejs/rpg.js
@@ -1,20 +1,76 @@
 var ts = new Tileset("tileset/mine.png");
-var map = new Map("000");
+var joueur = new Personnage("Adam.png",0,0,0);
+var map = new Map("000",joueur);
+var DUREE_ANIMATION = 4;
+var DUREE_DEPLACEMENT = 12;
+var ctx;
+var isPreCombat=false;
+var animID;
 
-window.onload = function() {
-	var canvas = document.getElementById('canvas');
-	var ctx = canvas.getContext('2d');
-        window.onkeydown = function(event) {
-	var e = event || window.event;
-        var key = e.which || e.keyCode;
-        alert(key);
-        return false;
-        };
-	
+function mDeplacement (ctx) {
+    
+        
+
 	canvas.width  = map.getLargeur() * 32;
 	canvas.height = map.getHauteur() * 32;
 	
-        var player = new Personnage("Adam.png", 1, 1, 0);
-        map.addPersonnage(player);
-	map.dessinerMap(ctx);
+	animID=setInterval(function() {
+            map.dessinerMap(ctx);
+        }, 40);
+        
+        // Gestion du clavier
+    window.onkeydown = function(event) {
+	
+        var e = event || window.event;
+        var key = e.which || e.keyCode;
+        
+        switch(key) {
+	case 38 : case 122 : case 119 : case 90 : case 87 : // Flèche haut, z, w, Z, W
+		joueur.deplacer(3,map);
+		break;
+	case 40 : case 115 : case 83 : // Flèche bas, s, S
+		joueur.deplacer(0,map);
+		break;
+	case 37 : case 113 : case 97 : case 81 : case 65 : // Flèche gauche, q, a, Q, A
+		joueur.deplacer(1,map);
+		break;
+	case 39 : case 100 : case 68 : // Flèche droite, d, D
+		joueur.deplacer(2,map);
+		break;
+	default : 
+		//alert(key);
+		// Si la touche ne nous sert pas, nous n'avons aucune raison de bloquer son comportement normal.
+		return true;
+        }
+	return false;
+    };
+    
+}
+function mPreCombat (ctx,mob) {
+    
+        if (!isPreCombat){
+        isPreCombat= true; 
+        
+	canvas.width  = map.getLargeur() * 32;
+	canvas.height = map.getHauteur() * 32;
+	
+//	setInterval(function() {
+//            map.dessinerMap(ctx);
+//        }, 40);
+        
+        // Gestion du clavier
+        window.onkeydown = function() {
+            return false;
+        };
+        mob.agro(joueur,map);
+        
+    }
+    
+}
+
+window.onload = function() {
+	var canvas = document.getElementById('canvas');
+	ctx = canvas.getContext('2d');
+        mDeplacement (ctx);
+	
 };
