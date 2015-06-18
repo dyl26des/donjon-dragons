4ec33f5f0434126d89e109ca09a0b8f3c691ce6b
Kaethuseth <gdigor@live.be>
2015-06-18 16:36:33 +0200

    finalisation "fin de combat"

--- classejs/combat.js
+++ classejs/combat.js
@@ -1,8 +1,4 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 function character (vie,armure,degats)
 {
     this.vie = vie;
@@ -19,10 +15,7 @@ function attaque()
 function fuir()
 {
     isPreCombat = false;
-    window.onkeydown = function() {
-            return true;
-        };
-    //alert(isPreCombat+' '+window.onkeydown());
+    alert(isPreCombat+' '+window.onkeydown());
     //map.getPersos().splice(map.getPersos().indexOf(mob),1);
     $('#combat').stop().hide();
     $('#canvas').stop().show();
--- classejs/rpg.js
+++ classejs/rpg.js
@@ -66,14 +66,45 @@ function mPreCombat (ctx,mob) {
         window.onkeydown = function() {
             return false;
         };
+        
         mob.agro(joueur,map);
-        //map.getPersos().splice(map.getPersos().indexOf(mob),1);
-        //setTimeout(map.getPersos().splice(map.getPersos().indexOf(mob),1),10000);
-        setTimeout("combat()",1000);
-        setTimeout(map.getPersos().splice(map.getPersos().indexOf(mob),1),1000);
-        window.onkeydown = function() {
-            return true;
-        };
+        
+        
+        function popcombat()
+        {
+            map.getPersos().splice(map.getPersos().indexOf(mob),1);
+            combat();
+            window.onkeydown = function(event) {
+	
+                    var e = event || window.event;
+                    var key = e.which || e.keyCode;
+                    switch(key) {
+                    //case 38 a 39, gestion des deplacement
+                    case 38 : case 122 : case 119 : case 90 : case 87 : // Flèche haut, z, w, Z, W
+                            joueur.deplacer(3,map);
+                            break;
+                    case 40 : case 115 : case 83 : // Flèche bas, s, S
+                            joueur.deplacer(0,map);
+                            break;
+                    case 37 : case 113 : case 97 : case 81 : case 65 : // Flèche gauche, q, a, Q, A
+                            joueur.deplacer(1,map);
+                            break;
+                    case 39 : case 100 : case 68 : // Flèche droite, d, D
+                            joueur.deplacer(2,map);
+                            break;
+                    //gestions des menu
+                    default : 
+                            //alert(key);
+                            // Si la touche ne nous sert pas, nous n'avons aucune raison de bloquer son comportement normal.
+                            return true;
+                    }
+                    return false;
+                };
+        }
+        
+        setTimeout(function(){popcombat();},2000);
+        
+        
     }
     
 }
