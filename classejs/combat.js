9e920f897818212b6c8e5de58bfa93b87108c987
dyl26des <dyl26des@gmail.com>
2015-06-18 20:42:49 +0200

    finalisation combat

--- classejs/combat.js
+++ classejs/combat.js
@@ -1,7 +1,5 @@
-
-
 var player ;
-var ennemy;
+var enemy;
 var ctx;
 function attaque(attaquant, defenseur)
 {
@@ -11,14 +9,14 @@ function attaque(attaquant, defenseur)
     if ((defenseur.vie - dmgfait) <= 0){
         
         
-            $('#pvennemy').html("0");
+            $('#pvenemy').html("0");
             alert("le " + defenseur.nom + " a subi trop de dommage pour \n\
     survivre");
         fincombat();
     }else
     {   
         defenseur.vie -= dmgfait;
-        $('#pvennemy').html(defenseur.vie);
+        $('#pvenemy').html(defenseur.vie);
         
         //ataque de l'ennemi
         dmgfait = defenseur.dmg * (1.5/attaquant.armor);
@@ -37,21 +35,45 @@ function attaque(attaquant, defenseur)
 function fincombat()
 {
     isPreCombat = false;
+    $('#texteCombat').html("");
     $('#combat').stop().hide();
     $('#canvas').stop().show();
     mDeplacement (ctx);
+ }
+ function fuir()
+ {
+    if(Math.floor(Math.random()) >0.6){
+        alert("vous avez fuit avec succes !");
+        fincombat();
+    }else
+    {
+        alert("vous n'avez pas couru assez vite!");
+        var dmgfait = enemy.dmg * (1.5/player.armor);
+        $('#texteCombat').append(enemy.nom + "ataque infligeant " +dmgfait+"dmg a " + player.nom);
+
+        if ((player.vie - dmgfait)<=0) {
+            $('#pvjoueur').html("0");
+            alert('le joueur est mort (0pv) vous avez perdu la parite !');
+            fincombat();
+        }else{
+            player.vie-=dmgfait;
+            $('#pvjoueur').html(player.vie);
+        }
+    }
  }
  
     function combat(mob,joueur,context)
 {
    player = joueur;
-   ennemy = mob;
+   enemy = mob;
    ctx= context;
+   $('#nomjoueur').html(player.nom);
+   $('#nomennemi').html(player.nom);
    $('#canvas').stop().hide();
    $('#combat').stop().show();
-   $('#texteCombat').append("un "+ ennemy.nom +" sauvage apparait.");  
+   $('#texteCombat').append("un "+ enemy.nom +" sauvage apparait.");  
    $('#attaqueButton' ).stop().click(function() {
-           attaque(player,ennemy);
+           attaque(player,enemy);
     });
    $('#fuirButton' ).stop().click(function() {
            fuir();
